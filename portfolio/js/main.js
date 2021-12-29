0e2a4fdb4bb474fcfe498f13857040ce79ab21d0
miiyi <92838696+miiyi@users.noreply.github.com>
2021-12-30 04:32:58 +0800

    fix mobile gotop

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,7 +1,14 @@
-$('#gotop').click(function(){
-    $('.container').animate({ scrollTop: 0 }, 'slow'); 
-    return false;
-});                    
+if( $(window).width() < 720 ){	
+    $('#gotop').click(function(){
+        $('html,body').animate({ scrollTop: 0 }, 'slow'); 
+        return false;
+    });
+}else{
+    $('#gotop').click(function(){
+        $('.container').animate({ scrollTop: 0 }, 'slow'); 
+        return false;
+    });
+}              
 setTimeout(function(){
     $(".cover").hide()
    },1500);
